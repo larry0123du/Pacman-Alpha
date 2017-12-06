77e812a834d0e6966d358a951c792c83771bee2f
Ray Du <du113@purdue.edu>
2017-12-05 22:30:31 -0500

    :(

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,13 @@ var mongodb = require('mongodb');
 var User = require('../models/user');
 var server = require('http').Server(router);
 var io = require('socket.io')(server);
+
+io.configure(function () {
+  io.set('transports', ['xhr-polling']);
+  io.set("polling duration", 10); 
+});
+
+
 var score;
 var Id;
 var spid;
